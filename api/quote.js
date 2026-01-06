0a443024d3b5490181f8b284cf822e933a3e90ab
abdulahadaa <abdulahadaa88345@gmail.com>
2026-01-06 14:57:10 +0500

    Update email password and add TLS config

--- api/quote.js
+++ api/quote.js
@@ -23,7 +23,10 @@ export default async function handler(req, res) {
       service: "gmail",
       auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        pass: process.env.EMAIL_PASS_NEW,
+      },
+      tls: {
+        rejectUnauthorized: false
       }
     });
 
