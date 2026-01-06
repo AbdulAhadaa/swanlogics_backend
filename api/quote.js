1fffd10a542fc6b3ca3286ad60932da6508ac3c9
abdulahadaa <abdulahadaa88345@gmail.com>
2026-01-06 12:47:17 +0500

    Fix both APIs for Vercel serverless deployment

--- api/quote.js
+++ api/quote.js
@@ -1,26 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-const dotenv = require("dotenv");
 const nodemailer = require("nodemailer");
 const emailTemplates = require("../templates/emailTemplates");
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use((req, res, next) => {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
-  next();
-});
-
-app.post("/quote", async (req, res) => {
+  
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
   
   const {
     service, projectTitle, projectDescription, budgetRange, preferredTimeline,
@@ -65,8 +57,4 @@ app.post("/quote", async (req, res) => {
     console.error("Quote error:", error);
     res.status(500).json({ error: "Failed to submit quote" });
   }
-});
-
-module.exports = (req, res) => {
-  return app(req, res);
 };
