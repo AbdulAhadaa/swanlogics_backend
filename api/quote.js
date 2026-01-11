4b3500e03bdf1919ca0c3d041dc0c01419c8adc5
abdulahadaa <abdulahadaa88345@gmail.com>
2026-01-11 13:26:39 +0500

    Force rebuild for quote endpoint

--- api/quote.js
+++ api/quote.js
@@ -1,5 +1,6 @@
 const sgMail = require('@sendgrid/mail');
 
+// Force rebuild - using SendGrid for email delivery
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
